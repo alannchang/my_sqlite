082ba986773bbc08226577a1e9b6dff029c1efa5
alan chang <chang_a@qwasar.io>
2023-08-11 22:18:57 +0000

    join works

--- my_sqlite_cli.rb
+++ my_sqlite_cli.rb
@@ -7,6 +7,8 @@ class MySqliteCli < MySqliteRequest
 end
 
 
+query = nil
+
 while cmd_line = Readline.readline("my_sqlite_cli>", true)
     
     if cmd_line.downcase == "quit"
@@ -18,26 +20,36 @@ while cmd_line = Readline.readline("my_sqlite_cli>", true)
         # find main .csv file (FROM, INSERT, UPDATE)
         if cmd_arr.include?("FROM")
             i_from = cmd_arr.index("FROM")
-            query = MySqliteCli.new.from(cmd_arr[i_from + 1])
+            if query == nil
+                query = MySqliteCli.new.from(cmd_arr[i_from + 1])
+            else 
+                query = query.from(cmd_arr[i_from + 1])
+            end
             cmd_arr.delete_at(i_from + 1) # csv file
             cmd_arr.delete_at(i_from) # "FROM"
             
         elsif cmd_arr.include?("INSERT")
             i_insert = cmd_arr.index("INSERT")
-            query = MySqliteCli.new.insert(cmd_arr[i_insert + 2])
+            if query == nil
+                query = MySqliteCli.new.insert(cmd_arr[i_insert + 2])
+            else
+                query = query.insert(cmd_arr[i_insert + 2])
+            end
             cmd_arr.delete_at(i_insert + 2) # csv file
             cmd_arr.delete_at(i_insert + 1) # "INTO"
             cmd_arr.delete_at(i_insert) # "INSERT"
 
         elsif cmd_arr.include?("UPDATE")
             i_update = cmd_arr.index("UPDATE")
-            query = MySqliteCli.new.update(cmd_arr[i_update + 1])
+            if query == nil
+                query = MySqliteCli.new.update(cmd_arr[i_update + 1])
+            else
+                query = query.update(cmd_arr[i_update + 1])
+            end
             cmd_arr.delete_at(i_update + 1) # csv file
             cmd_arr.delete_at(i_update) # "UPDATE"
 
         end
-
-        # cmd_arr.each { |element| puts element }
 
         # everything else (SELECT, WHERE, JOIN, ORDER, VALUES, SET, DELETE)
         cmd_arr.each_with_index do |element, index|
@@ -64,13 +76,19 @@ while cmd_line = Readline.readline("my_sqlite_cli>", true)
                 end
             
             when "WHERE"
-                
                 column_name = cmd_arr[index + 1]
                 criteria = cmd_arr[index + 3][1..-2]
                 query = query.where(column_name, criteria)
             when "JOIN"
+                db2 = cmd_arr[index + 1]
+                column_a = cmd_arr[index + 3]
+                column_b = cmd_arr[index + 5]
+                query = query.join(column_a, db2, column_b)
+
             when "ORDER"
+                
             when "VALUES"
+
             when "SET"
                 data = {}
                 column = cmd_arr[index + 1]
@@ -86,8 +104,6 @@ while cmd_line = Readline.readline("my_sqlite_cli>", true)
         end
             
 
-
-
     end
 
 end
@@ -99,15 +115,15 @@ end
 # SELECT Gender, Email, UserName, Device FROM small_test.csv;
 
 # SELECT * FROM small_test.csv WHERE FirstName = 'Carl';
-# SELECT Gender, LastName, UserName, Age FROM small_test.csv WHERE Device = 'Chrome Android';
+# SELECT Gender, LastName, UserName, Age FROM small_test.csv WHERE Device = 'Chrome_Android';
 
-# SELECT Player, height, position FROM nba_players.csv JOIN nba_players.csv ON Player = name;
+# SELECT Player, height, position FROM nba_players.csv JOIN nba_player_data.csv ON Player = name;
 
 # SELECT * FROM small_test.csv ORDER BY Age ASC;
 # SELECT * FROM nba_players.csv ORDER BY born DESC;
 
-# INSERT INTO small_test.csv VALUES Gender = 'Male', FirstName = 'John', LastName = 'Smith', UserName = 'john', Email = 'john.smith.aol.com', Age = '90', City = 'San Andreas', Device = 'Chrome Android', Coffee Quantity = '1', Order At = '1990-10-25 5:23:51';
+# INSERT INTO small_test.csv VALUES Gender = 'Male', FirstName = 'John', LastName = 'Smith', UserName = 'john', Email = 'john.smith.aol.com', Age = '90', City = 'San_Andreas', Device = 'Chrome_Android', Coffee_Quantity = '1', Order_At = '1990-10-25_5:23:51';
 
-# UPDATE small_test.csv SET Coffee Quantity = '99' WHERE Gender = "Male";
+# UPDATE small_test.csv SET Coffee_Quantity = '99' WHERE Gender = "Male";
 
 # DELETE FROM small_test.csv WHERE Gender = 'Female' AND Age = '21';
